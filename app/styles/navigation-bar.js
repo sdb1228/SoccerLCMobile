c0c7625d9359be357571bb3d25b4974602c9f319
steven burnett <stevendburnett@gmail.com>
2016-10-19 13:53:15 -0600

    forgot files

---
+++ app/styles/navigation-bar.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  tab: {
+    flex: 1,
+    paddingTop: 15,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  tabs: {
+    height: 55,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: 'rgba(0,0,0,0.05)',
+  },
+})
