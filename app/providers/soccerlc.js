9a2078de761a677e87ddb851264db5705244a2e5
Felix Milea-Ciobanu <felixmilea@gmail.com>
2016-10-19 23:06:57 -0600

    response intercetor for soccerlc provider

--- app/providers/soccerlc.js
+++ app/providers/soccerlc.js
@@ -1,6 +1,12 @@
-import axios from 'axios'
+import Axios from 'axios'
 
 import { soccerlc } from './config'
+
+// create our own instance of axios for SoccerLC API so that way we can configure it
+const axios = Axios.create()
+
+// remove a layer of .data from the response
+axios.interceptors.response.use(res => res.data, err => Promise.reject(error))
 
 export function getFacilityTeams (facilityId) {
   return axios.get(`${soccerlc.baseUrl}facilities/${facilityId}/teams`)
